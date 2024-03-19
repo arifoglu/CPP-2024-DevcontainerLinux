2382bb711be07062ae07d20d81f0c68bf0d01824
arifoglu <arifoglu2020@gmail.com>
2024-03-19 10:07:13 +0000

    basic concepts has been seen

---
+++ part1/fourth.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+
+int main(){
+    int slices = 4;
+    cout << "you have " << slices << " slices" <<std::endl;
+
+    printf("%d\n",slices);
+
+    return 0;
+}
---
+++ part1/userinput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int slices;
+    cout << "type a number " <<endl;
+    cin  >> slices ;
+    cout << "you have " << slices << " slices" <<endl;
+    return 0;
+}
