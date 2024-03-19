cf73b31fd8bb48831623df9fa2ce1b894e40c5aa
arifoglu <arifoglu2020@gmail.com>
2024-03-19 10:21:26 +0000

    2 project has been done

---
+++ w3resource/2/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "type a first number :" ;
+    cin  >> a ;
+    cout << "type a second number :";
+    cin  >> b;
+    int sum = a + b ;
+    cout << a << "+" << b << "=" <<sum <<endl; 
+}
