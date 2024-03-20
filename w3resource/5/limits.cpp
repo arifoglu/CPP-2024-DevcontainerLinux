4a557b00465609e44c4bc47475edcddcdf37b026
arifoglu <arifoglu2020@gmail.com>
2024-03-20 10:38:03 +0000

    climits has been done

---
+++ w3resource/5/limits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    cout << INT_MAX  <<endl;
+    cout << INT_MIN  <<endl;
+    cout << CHAR_BIT <<endl;
+    cout << CHAR_MAX <<endl;
+    cout << CHAR_MIN <<endl;
+    return 0;
+}
