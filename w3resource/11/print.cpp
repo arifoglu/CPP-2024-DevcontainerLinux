e4e5ca0aa5a968bc71a15f5630375f0b4badb308
arifoglu <arifoglu2020@gmail.com>
2024-03-26 10:22:14 +0000

    11 has been done

---
+++ w3resource/11/print.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Result of 1st expression is " << ((3 * 5) + 8) << std::endl; 
+    std::cout << "Result of 1st expression is " << ((2 * 2) + 1) << std::endl; 
+    std::cout << "Result of 1st expression is " << ((3 * 6) - 6) << std::endl; 
+    std::cout << "Result of 1st expression is " << ((3 * 5) % 4) << std::endl; 
+    return 0;
+}
