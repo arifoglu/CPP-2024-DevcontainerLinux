360ce8312f397d6804f0ca0fac08121f08fc2a1f
arifoglu <arifoglu2020@gmail.com>
2024-04-09 08:33:01 +0000

    guess game done

---
+++ part7/game.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+void play_game();
+
+int main(){
+    srand(time(NULL));
+
+    std::cout << "Do you want to play a game? \n0.Quit game\n1.Play game\n";
+    int choice;
+    std::cin >> choice ;
+
+    do{
+        switch(choice){
+           case 0:
+             std::cout << "Ok.Another time " <<std::endl;
+             break;
+           case 1:
+             std::cout << "Let's start " <<std::endl;
+             play_game();
+             break;
+        }
+
+    }while(choice != 0);
+
+    
+    return 0;
+}
+
+void play_game(){
+    int random = rand() % 251;
+    //std::cout << random <<std::endl;
+    std::cout << "Guess a number : " <<std::endl;
+    while(true){
+        int guess;
+        std::cin >> guess;
+        if(guess == random){
+             std::cout << "You win!\n " <<std::endl;
+             play_game();
+        }else if (guess < random ){
+             std::cout << "Too low!\n " <<std::endl;
+        }else{
+             std::cout << "Too high!\n " <<std::endl;
+        }
+    };
+    
+}
