e7a3ffe3e7b2d2e2f79f9931d1e40186872d1566
arifoglu <arifoglu2020@gmail.com>
2024-04-09 09:25:38 +0000

    31 has been done

---
+++ w3resource/31/shape.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int num;
+    std::cout << "Enter a number : " <<std::endl;
+    std::cin  >> num ;
+
+    std::cout << " " << num << num << num << num <<std::endl; 
+    std::cout << " " << num << " " << " " << num <<std::endl; 
+    std::cout << " " << num << " " << " " << num <<std::endl; 
+    std::cout << " " << num << " " << " " << num <<std::endl; 
+    std::cout << " " << num << " " << " " << num <<std::endl;
+    std::cout << " " << num << num << num << num <<std::endl; 
+
+    std::cout << "***********************" <<std::endl;
+
+    return 0;
+}
