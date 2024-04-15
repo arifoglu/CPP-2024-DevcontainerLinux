b48bb81a118c9f0375d7a119946944e1122e33c9
arifoglu <arifoglu2020@gmail.com>
2024-04-15 09:01:14 +0000

    42 has been done

---
+++ w3resource/42/reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string first_name,last_name;
+    std::cout << "First Name :";
+    std::cin  >> first_name ;
+    std::cout << "Last Name :";
+    std::cin  >> last_name ;
+
+    std::string other = first_name;
+
+    first_name = last_name;
+    last_name = other;
+
+    std::cout << "First Name : " << first_name << " " << "Last Name : " << other << std::endl;
+
+}
