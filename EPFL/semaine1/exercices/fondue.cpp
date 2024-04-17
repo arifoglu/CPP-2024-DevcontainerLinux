c8e8571ad6bc9912094915ff6476844466c09fe0
arifoglu <arifoglu2020@gmail.com>
2024-04-17 11:28:27 +0000

    exercice fondue has been done

---
+++ EPFL/semaine1/exercices/fondue.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(){
+    int BASE = 4;
+    double fromage = 800.0;
+    double eau = 2.0;
+    double ail = 2.0;
+    double pain = 400.0;
+
+    int nb_convives;
+    std::cout << "Etes-vous combien de personnes? :";
+    std::cin >> nb_convives;
+
+    double nouvelle_quantite_fromage = (fromage * nb_convives) / BASE;
+    double nouvelle_quantite_eau = (eau * nb_convives) / BASE;
+    double nouvelle_quantite_ail = (ail * nb_convives) / BASE;
+    double nouvelle_quantite_pain = (pain * nb_convives) / BASE;
+    std::cout << "Pour faire une fondue ; il vous faut : " <<std::endl;
+    std::cout << nouvelle_quantite_fromage << " gr fromage" <<std::endl;
+    std::cout << nouvelle_quantite_eau     << " dl d'eau " <<std::endl;
+    std::cout << nouvelle_quantite_ail     << " gousse d'ail " <<std::endl;
+    std::cout << nouvelle_quantite_pain    << " gr de pain" <<std::endl;
+    
+
+    return 0;
+}
