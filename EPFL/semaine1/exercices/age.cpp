ae0f1d91d0db5283b50a6915b8764d5eb7cf28f0
arifoglu <arifoglu2020@gmail.com>
2024-04-17 11:13:43 +0000

    exercices age have been done

--- EPFL/semaine1/exercices/age.cpp
+++ EPFL/semaine1/exercices/age.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <ctime>
 
 int main(){
+    time_t t = time(NULL);
+    tm *tPtr = localtime(&t);
+
+    int year = (tPtr->tm_year) + 1900 ;
+    std::cout << year <<std::endl;
+
     int votre_age ,annee;
 
     std::cout << "Entré votre âge : " ;
     std::cin  >> votre_age;
+    
+    int naissance = year - votre_age ;
 
-    std::cout << 
+    std::cout << "Votre date de naissance : "  << naissance <<std::endl;
+    
 
 
 
--- EPFL/semaine1/exercices/imc.cpp
+++ EPFL/semaine1/exercices/imc.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     double poids,taille,imc;
-    
+    std::cout << "Pour trouver indice de masse corporelle veuillez indiquez votre poids et taille svp" <<std::endl; 
     std::cout << "Poids : " ;
     std::cin  >> poids;
     std::cout << "Taille : " ;
@@ -13,7 +13,19 @@ int main(){
     imc = poids / (pow(taille,2)) ; 
 
     std::cout << "Votre imc (indice de masse corporelle) : "<< std::setprecision(4) <<imc <<std::endl;
-
+    if(imc < 18.5){
+        std::cout << "Sous la normale" <<std::endl;
+    }else if(imc >= 18.5 && imc <24.9){
+        std::cout << "Etendue normale" <<std::endl;
+    }else if(imc >= 24.9 && imc <29.9){
+        std::cout << "surpoids" <<std::endl;
+    }else if(imc >= 29.9 && imc <34.9){
+        std::cout << "Obèse 1" <<std::endl;
+    }else if(imc >= 34.9 && imc <39.9){
+        std::cout << "Obèse 2 sévère" <<std::endl;
+    }else if(imc >= 40){
+        std::cout << "Obèse 3 morbide" <<std::endl;
+    }
 
     return 0;
 }
