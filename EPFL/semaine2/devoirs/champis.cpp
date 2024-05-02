d536cd3da0133cffc37a0ea3c7a6c15da52c37a3
arifoglu <arifoglu2020@gmail.com>
2024-05-02 12:22:54 +0000

    champis.cpp have been done

--- EPFL/semaine2/devoirs/champis.cpp
+++ EPFL/semaine2/devoirs/champis.cpp
@@ -2,30 +2,64 @@
 using namespace std;
 
 int main(){
-  cout << "Pensez à un champignon : amanite tue-mouches, pied bleu, girolle," << endl
+    int lamelles,anneau,chapeau_convexe,foret;
+    cout << "Pensez à un champignon : amanite tue-mouches, pied bleu, girolle," << endl
        << "cèpe de Bordeaux, coprin chevelu ou agaric jaunissant." << endl << endl;
-       
+    
+    cout << "Est-ce que votre champignon a un chapeau convexe (1 : oui, 0 : non) ? ";
+    cin >> chapeau_convexe;
+    if(chapeau_convexe)
+    {
+      cout << "Est-ce que votre champignon vit en forêt (1 : oui, 0 : non) ? ";
+      cin >> foret;
 
+       if(foret)
+       {
+       cout << "Est-ce que votre champignon a un anneau (1 : oui, 0 : non) ? ";
+       cin >> anneau ;
 
-  return 0;
+          if(anneau)
+            {
+              cout << "==> Le champignon auquel vous pensez est  l'amanite tue-mouches.\n"; 
+            }
+            else if(!anneau)
+            {
+              cout << "==> Le champignon auquel vous pensez est  le pied bleu.\n";
+            }
+          }
+          else if(!foret)
+          {
+              cout << "==> Le champignon auquel vous pensez est  l'agaric jaunissant.\n";
+          }
+    }
+    else if (!chapeau_convexe)
+    {
+      cout << "Est-ce que votre champignon vit en forêt (1 : oui, 0 : non) ? ";
+      cin >> foret;
+
+        if(foret)
+        {
+           cout << "Est-ce que votre champignon a des lamelles (1 : oui, 0 : non) ? ";
+           cin >> lamelles;
+
+             if(lamelles)
+             {
+              cout << "==> Le champignon auquel vous pensez est  la girolle.\n";
+             } 
+             else if(!lamelles)
+             {
+                cout << "==> Le champignon auquel vous pensez est le cèpe de Bordeaux.\n";
+             }
+        }
+        else if(!foret)
+        {
+         cout << "==> Le champignon auquel vous pensez est  le coprin chevelu.\n";
+        }
+    }
+    return 0;
 }
 
 
-/*
-  cout << "Pensez à un champignon : amanite tue-mouches, pied bleu, girolle," << endl
-       << "cèpe de Bordeaux, coprin chevelu ou agaric jaunissant." << endl << endl;
-  
-  cout << "Est-ce que votre champignon a des lamelles (1 : oui, 0 : non) ? ";
-  cout << "Est-ce que votre champignon a un anneau (1 : oui, 0 : non) ? ";
-  cout << "Est-ce que votre champignon a un chapeau convexe (1 : oui, 0 : non) ? ";
-  cout << "Est-ce que votre champignon vit en forêt (1 : oui, 0 : non) ? ";
+     
 
-  cout << "==> Le champignon auquel vous pensez est ";
 
-  cout << "l'agaric jaunissant";
-  cout << "l'amanite tue-mouches";
-  cout << "le cèpe de Bordeaux";
-  cout << "le coprin chevelu";
-  cout << "la girolle";
-  cout << "le pied bleu";
-*/
