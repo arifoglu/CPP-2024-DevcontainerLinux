f5d11346edad51cd3b43a41b206bb0a58cc2bc58
“abdulkadir” <arifoglu2020@gmail.com>
2024-05-30 13:34:23 +0200

    rebonds done

--- EPFL/semaine3/exercices/rebonds.cpp
+++ EPFL/semaine3/exercices/rebonds.cpp
@@ -5,20 +5,34 @@ using namespace std;
 
 int main(){
     const double g = 9.81;
-    double H0,eps,NBR ;
-    cout << "Entrez la hauteur initiale( H0 > O) : " <<endl;
-    cin >> H0 ;
-    cout << "Entrez coefficient de rebonds( 0 <= eps  1) : " <<endl;
-    cin >> eps ;
-    cout << "Entrez le nombre de rebonds( nbr > O) : " <<endl;
-    cin >> NBR ;
+    double h0,eps,n ;
+
+    do{
+        cout << "Hauteur initiale (h0 > 0) : ";
+        cin >> h0;  
+    }while(h0 <= 0.0);
+
+    do{
+        cout << "Coefficient de rebond (0 <= coeff < 1 ) : ";
+        cin >> eps;
+    }while((eps < 0.0) | (eps >= 1.0));
+
+    do{
+        cout << "Nombre de rebonds (n >= 0) : ";
+        cin >> n;
+    }while(n < 0);
     
-    double V = sqrt(2 * H0 * g);
-    double V1 = eps * V ;
-    double H1 = pow(V1,2)/ 2 * g; 
-    cout << "V = " << V << endl;
-    cout << "V1 = " << V1 << endl;
-    cout << "H1 = " << H1 << endl;
-    
+    double h1,v0,v1;
+
+    for(int rebonds = 1; rebonds <= n ;++rebonds){
+        v0 = sqrt(2 * h0 * g);
+        v1 = eps * v0;
+        h1 = pow(v1,2) / (2.0 * g);
+        
+        h0 = h1;
+
+        cout << "Au " << rebonds << "e rebond ,la hauteur atteinte de " << h0 <<endl;
+    }
+   
     return 0;
 }
