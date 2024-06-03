1865ac7697e35d4171128739994ba3a7ae979ddb
“abdulkadir” <arifoglu2020@gmail.com>
2024-06-03 13:36:47 +0200

    etoile added

--- EPFL/semaine3/exercices/etoile.cpp
+++ EPFL/semaine3/exercices/etoile.cpp
@@ -5,11 +5,13 @@ int main(){
     int n;
     cout << "Entrez numero de la ligne: ";
     cin >> n;
-    for(int i = 0;i < n;i++){
-       for(int j = 0;j < n - i ;j++){
-        cout << "";
+    for(int i = 0;i < n;++i){
+       for(int j = 1;j < n - i ;++j){
+        cout << "+";
        }
-       
+       for(int k = 0; k < 2 * i ;++k){
+        cout << "*";
+       }
        cout << endl;
     }
     return 0;
