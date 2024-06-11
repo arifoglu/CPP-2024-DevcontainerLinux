1aa7c5dd96c1352176459042c3a3910f40be7175
“abdulkadir” <arifoglu2020@gmail.com>
2024-06-11 11:01:47 +0200

    78 done

---
+++ w3resource/78/sum.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <sstream>  // Include this for stringstream
+
+using namespace std;
+
+int main() {
+    string str;
+    int sum = 0, num;
+
+    // Read input line by line
+    while (getline(cin, str)) {
+        // Convert all non-digit characters to spaces
+        for (int i = 0; i < (int)str.size(); i++) {
+            if (!isdigit(str[i])) {
+                str[i] = ' ';
+            }
+        }
+
+        // Use stringstream to extract numbers from the modified string
+        stringstream abc(str);
+
+        // Extract and sum up all integers
+        while (abc >> num) {
+            sum += num;
+        }
+    }
+
+    cout << "Sum of all numbers: " << sum << endl;
+    return 0;
+}
