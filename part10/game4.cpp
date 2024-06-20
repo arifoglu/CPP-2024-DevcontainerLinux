4eea4c24c27b8e0fb7da6cc927b1a6cf86c4554d
“abdulkadir” <arifoglu2020@gmail.com>
2024-06-20 11:35:07 +0200

    game  done

--- part10/game4.cpp
+++ part10/game4.cpp
@@ -4,15 +4,62 @@
 #include <cstdlib>
 #include <ctime>
 #include <array>
-
-void play_game();
+#include <fstream>
 
 void print_vector(std::vector<int> vector){
     for(int i = 0 ;i < vector.size() ;i++){
         std::cout << vector[i] << "\t";
     }
     std::cout << "\n";
+}; 
+
+void play_game(){
+    std::vector<int> guesses;
+    int count = 0;
+
+    int random = rand() % 101;
+    //std::cout << random <<std::endl;
+    std::cout << "Guess a number(0-100) : " <<std::endl;
+    while(true){
+        int guess;
+        std::cin >> guess;
+        count++;
+        guesses.push_back(guess);
+
+        if(guess == random){
+             std::cout << "You win!\n " <<std::endl;
+             play_game();
+        }else if (guess < random ){
+             std::cout << "Too low!\n " <<std::endl;
+        }else{
+             std::cout << "Too high!\n " <<std::endl;
+        }
+
+        std::ifstream input("best_score.txt");
+
+        if(!input.is_open()){
+            std::cout << "unable to read file\n";
+            return;
+        } 
+
+        int best_score;
+        input >> best_score;
+
+        std::ofstream output("best_score.txt");
+        if(!output.is_open()){
+            std::cout << "Unable to read file\n";
+            return;
+        } 
+        if(count < best_score){
+            output << count;
+        }else{
+            output << best_score;
+        }
+        print_vector(guesses);
+    };
+    
 };
+
 
 int main(){
     srand(time(NULL));
@@ -37,29 +84,5 @@ int main(){
     return 0;
 };
 
-void play_game(){
-    std::vector<int> guesses;
-    int count = 0;
 
-    int random = rand() % 101;
-    //std::cout << random <<std::endl;
-    std::cout << "Guess a number(0-100) : " <<std::endl;
-    while(true){
-        int guess;
-        std::cin >> guess;
-    
-        guesses.push_back(guess);
-
-        if(guess == random){
-             std::cout << "You win!\n " <<std::endl;
-             play_game();
-        }else if (guess < random ){
-             std::cout << "Too low!\n " <<std::endl;
-        }else{
-             std::cout << "Too high!\n " <<std::endl;
-        }
-        print_vector(guesses);
-    };
-    
-};
 
